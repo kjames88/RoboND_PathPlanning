482faae0d0256f960c98c0906de10b829848af4d
Kevin James <kjames4@gmail.com>
2018-11-12 22:48:02 -0800

    Update script

--- ShellScripts/test_slam.sh
+++ ShellScripts/test_slam.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-xterm -e " roslaunch turtlebot_simulator/turtlebot_gazebo/launch/turtlebot_world.launch " &
+xterm -e " roslaunch turtlebot_simulator/turtlebot_gazebo/launch/turtlebot_world.launch world_file:=/home/kjames/catkin_ws/src/World/corridor_l " &
 sleep 5
 xterm -e " roslaunch turtlebot_simulator/turtlebot_gazebo/launch/gmapping_demo.launch " &
 sleep 5
